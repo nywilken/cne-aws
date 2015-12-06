81689a40e3f1e80ff17d84e28d7bbc64a128d4ce
jpcallanta <jp@callanta.net>
2015-12-06 17:48:56 -0500

    Adding functionality to list security groups and regex search by name

--- README.md
+++ README.md
@@ -1,19 +1,27 @@
 # cne-aws
 CNE AWS command line utility
 
-# Setup
+## Setup
 These environment variables must be set in your terminal before usage:
 
 `AWS_REGION` `AWS_ACCESS_KEY_ID` `AWS_SECRET_ACCESS_KEY`
 
-# Building and installing the gem locally
+## Building and installing the gem locally
 Tested on ruby-2.1.2
 
 To build and install:
 `gem build cne-aws.gemspec && gem install cne-aws-<version>.gem`
 
-# Building and installing the gem via repo
+## Building and installing the gem via repo
 `gem install specific_install && gem specific_install -l https://github.com/jpcallanta/cne-aws`
 
-# Usage
+## Usage
+
+#### Lists instances based on tag
 `cne instances [appname_tag_val] [env_tag_val]`
+
+#### Lists all security groups
+`cne security`
+
+#### Lists security groups based on search param regex
+`cne security [search param]`
--- lib/cne.rb
+++ lib/cne.rb
@@ -63,4 +63,56 @@ class Cne
     puts "#{instance_count} instances".colorize(:red)
     puts ''
   end
+
+  def list_security_groups(name)
+    output = []
+    response = @ec2_client.describe_security_groups
+
+    if name.empty?
+      response.security_groups.each do |group|
+        output << [
+          "#{group.group_id}".colorize(:green),
+          "#{group.group_name}".colorize(:green),
+          "#{group.description}".colorize(:green)
+        ]
+      end
+
+      table = Terminal::Table.new(
+        :headings => [
+          'Group ID'.colorize(:blue),
+          'Group Name'.colorize(:blue),
+          'Group Description'.colorize(:blue)
+        ],
+        :rows => output.sort_by! { |name| name[1] }
+      )
+
+      puts table
+    else
+      response.security_groups.each do |group|
+        if group.group_name =~ /#{name}/
+          output << [
+            "#{group.group_id}".colorize(:green),
+            "#{group.group_name}".colorize(:green),
+            "#{group.description}".colorize(:green)
+          ]
+        end
+      end
+
+      if output.empty?
+        puts 'No security groups found...'.colorize(:red)
+        exit 1
+      end
+
+      table = Terminal::Table.new(
+        :headings => [
+          'Group ID'.colorize(:blue),
+          'Group Name'.colorize(:blue),
+          'Group Description'.colorize(:blue)
+        ],
+        :rows => output.sort_by! { |name| name[1] }
+      )
+
+      puts table
+    end
+  end
 end
